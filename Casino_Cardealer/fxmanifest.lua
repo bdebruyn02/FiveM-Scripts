4583c3213d88123d3e3b4986cf911426f04a7cc5
Brandon <31498965+LordTiger@users.noreply.github.com>
2021-03-04 22:21:23 +0200

    Added fxmanifest

---
+++ Casino_Cardealer/fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version "adamant"
+
+game "gta5"
+
+this_is_a_map "yes"
+
+author 'Discord: LordTiger#1913'
+description 'Join my Discord for more FiveM Scripts & Other Stuff'
+
+
+data_file 'DLC_ITYP_REQUEST' 'stream/builderdefaddons.ytyp'
